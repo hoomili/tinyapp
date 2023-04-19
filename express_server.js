d315a011a3324da33cf21ca5cc5b066eda2131d2
hoomili <hasadian@hotmail.com>
2023-04-19 13:18:53 -0700

    added morgan package

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 const PORT = 8080;
 
 
@@ -23,7 +24,7 @@ const generateRandomString = () => {
 app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send("Hello!");
