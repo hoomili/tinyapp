8b046c22640c9b9e418fea4dae0e643a71ec7c38
hoomili <hasadian@hotmail.com>
2023-04-19 13:22:30 -0700

    added user database

--- express_server.js
+++ express_server.js
@@ -10,6 +10,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// user database
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 // the function below makes a random string that has a length of 6 characters
 const generateRandomString = () => {
   let randomString = '';
